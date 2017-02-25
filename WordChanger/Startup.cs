5256e4e8cca896225bdd4b3c2ea310a61485e4bc
nghillaz <nghill2@asu.edu>
2017-02-25 12:47:49 -0700

    initial commit

---
+++ WordChanger/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WordChanger.Startup))]
+namespace WordChanger
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
