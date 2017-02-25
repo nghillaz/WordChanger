5a9abdcfecb41838680cce9060ec9f75f182983c
nghillaz <nghillaz@gmail.com>
2017-02-25 16:34:35 -0700

    fixed addword method

--- WordChanger/Default.aspx.cs
+++ WordChanger/Default.aspx.cs
@@ -7,10 +7,10 @@ using System.Text;
 
 namespace WordChanger
 {
-    class Word
+    public class Word
     {
         public string word;
-        public string[] synonyms;
+        public ArrayList synonyms;
     }
 
     public partial class _Default : Page
@@ -19,6 +19,8 @@ namespace WordChanger
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            WordList = new ArrayList();
+
             AddWord("dude");
         }
 
@@ -41,15 +43,15 @@ namespace WordChanger
             string[] formattedJson = jsonString.Split(',');
 
             Word newWord = new Word();
-            newWord.synonyms = new string[formattedJson.Length - 2];
-
+            newWord.synonyms = new ArrayList();
+            
             newWord.word = formattedJson[0].Substring(9, formattedJson[0].Length - 10);
-            newWord.synonyms[0] = formattedJson[1].Substring(13, formattedJson[1].Length - 14);
+            newWord.synonyms.Add(formattedJson[1].Substring(13, formattedJson[1].Length - 14));
             for (int i = 2; i < formattedJson.Length - 1; i++)
             {
-                newWord.synonyms[i - 1] = formattedJson[i].Substring(1, formattedJson[i].Length - 2);
+                newWord.synonyms.Add(formattedJson[i].Substring(1, formattedJson[i].Length - 2));
             }
-            newWord.synonyms[formattedJson.Length - 2] = formattedJson[formattedJson.Length - 1].Substring(1, formattedJson[formattedJson.Length - 1].Length - 4);
+            newWord.synonyms.Add(formattedJson[formattedJson.Length - 1].Substring(1, formattedJson[formattedJson.Length - 1].Length - 4));
 
             WordList.Add(newWord);
         }
